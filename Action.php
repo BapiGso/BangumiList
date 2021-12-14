2b41a0a9fd24198bd00c46a341d26130329ad681
BapiGso <59081856+BapiGso@users.noreply.github.com>
2021-12-14 15:07:53 +0800

    Add files via upload

---
+++ Action.php
@@ -0,0 +1,125 @@
+<?php
+
+class BangumiAPI
+{
+    private static $bangumiAPI = null;
+
+    private static $apiUrl = 'https://api.bgm.tv';
+
+    private $myCollection;
+
+    private $collectionApi = '';
+
+
+    public static function GetInstance()
+    {
+        if (BangumiAPI::$bangumiAPI == null) {
+            BangumiAPI::$bangumiAPI = new BangumiAPI();
+        }
+        return BangumiAPI::$bangumiAPI;
+    }
+
+    private function __construct()
+    {
+    }
+
+    public function initCollectionApi($userID,$appID)
+    {
+        $this->collectionApi = BangumiAPI::$apiUrl . '/user/' . $userID . '/collections/anime?app_id=' . $appID . '&max_results=99';
+    }
+
+    public function getCollection($userID, $appID,$hasCache, $cacheTime)
+    {
+        if (empty($userID)) return false;
+        $this->initCollectionApi($userID,$appID);
+        $FilePath = __DIR__ . '/bgm-list.json';
+        if ($hasCache && file_exists($FilePath) && time() - filemtime($FilePath) < $cacheTime) {
+            $file = fopen($FilePath, 'r');
+            if (!$this->verifyCollection(fread($file, filesize($FilePath)))) {
+                $data = BangumiAPI::curl_get_contents($this->collectionApi);
+                if (!$this->verifyCollection($data)) return false;
+                file_put_contents($FilePath, $data, LOCK_EX);
+            }
+            fclose($file);
+        } else {
+            $data = BangumiAPI::curl_get_contents($this->collectionApi);
+            if (!$this->verifyCollection($data)) return false;
+            file_put_contents($FilePath, $data, LOCK_EX);
+        }
+        return true;
+    }
+
+    public function verifyCollection($data)
+    {
+        $content = json_decode($data,true);
+        if (empty($content)) return false;
+        $index = 0;
+        foreach ($content as $value) {
+            $this->myCollection[$index++] = $value;
+            //var_dump($value);
+        }
+        return true;
+    }
+
+    public function printCollecion()
+    {
+        foreach ($this->myCollection as $value) {
+            // echo '<style>.Bangumi ul{display:inline-block;vertical-align:top;margin:10px 10px 10px 0}.Bangumi ul img{width:88px;height:132px;object-fit:cover}.Bangumi ul p{width:88px;display:block;margin-block-start:1em;margin-block-end:1em;margin-inline-start:0;margin-inline-end:0;overflow-wrap:break-word}</style>';
+            $watching = $value["collects"][0]["list"];//在看
+            $watched = $value["collects"][1]["list"];//看过
+            $wantsee = $value["collects"][2]["list"];//想看
+            //$watchhold = $value["collects"][3]["list"];//搁置
+            //$watchdrop = $value["collects"][4]["list"];//抛弃
+            $watchPlus = array_merge($watching,$watched);//限制25条把在看和看过合并为看过
+            array_multisort(array_column($watchPlus,'subject_id'),SORT_ASC,$watchPlus);//按subject_id排序(也就是时间排序)
+            array_multisort(array_column($wantsee,'subject_id'),SORT_ASC,$wantsee);//按subject_id排序(也就是时间排序)
+            echo "<br><b>看过</b><br>";
+            foreach ($watchPlus as $value1) {
+            $name = $value1["subject"]["name_cn"] ?: $value1["subject"]["name"];
+            $url = $value1["subject"]["url"];
+            $imgUrl = $value1["subject"]["images"]["medium"];
+            echo "<ul><a href=" . $url . " target='_blank' class='bangumi'><img src='$imgUrl'/></a><p>$name</p><br></ul>";
+            }
+            echo "<br><b>想看</b><br>";
+            foreach ($wantsee as $value1) {
+            $name = $value1["subject"]["name_cn"] ?: $value1["subject"]["name"];
+            $url = $value1["subject"]["url"];
+            $imgUrl = $value1["subject"]["images"]["medium"];
+            echo "<ul><a href=" . $url . " target='_blank' class='bangumi'><img src='$imgUrl'/></a><p>$name</p><br></ul>";
+            }
+        }
+    }
+
+    private static function curl_get_contents($_url)
+    {
+        $myCurl = curl_init($_url);
+        curl_setopt($myCurl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($myCurl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($myCurl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($myCurl, CURLOPT_HEADER, false);
+        $content = curl_exec($myCurl);
+        curl_close($myCurl);
+        //echo $content;
+        return $content;
+    }
+}
+
+class BangumiList_Action extends Widget_Abstract_Contents implements Widget_Interface_Do
+{
+
+    public function action()
+    {
+        $options = Helper::options();
+        $userID = trim($options->plugin('BangumiList')->userID);
+        $appID = $options->plugin('BangumiList')->appID;
+        $hasCache = $options->plugin('BangumiList')->hasCache;
+        $cacheTime = trim($options->plugin('BangumiList')->cacheTime);
+
+        $bangumi = BangumiAPI::GetInstance();
+        if ($bangumi->getCollection($userID, $appID,$hasCache, $cacheTime)) {
+            $bangumi->printCollecion();
+        } else {
+            echo '没有追番记录';
+        }
+    }
+}
---
+++ Plugin.php
@@ -0,0 +1,102 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+
+/**
+ * Bangumi 追番列表
+ *
+ * @package BangumiList
+ * @author SSpirits
+ * @version 1.0.0
+ * @link http://blog.sspirits.top
+ */
+class BangumiList_Plugin implements Typecho_Plugin_Interface
+{
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     *
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function activate()
+    {
+        Typecho_Plugin::factory('Widget_Abstract_Contents')->contentEx = array('BangumiList_Plugin', 'replace');
+        Helper::addRoute("route_BangumiList", "/BangumiList", "BangumiList_Action", 'action');
+    }
+
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     *
+     * @static
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate()
+    {
+        Helper::removeRoute("route_BangumiList");
+    }
+
+    public static function personalConfig(Typecho_Widget_Helper_Form $form) {}
+
+    /**
+     * 获取插件配置面板
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
+        /**表单设置 */
+        $userID = new Typecho_Widget_Helper_Form_Element_Text('userID', NULL, NULL, _t('输入你的 Bangumi 用户 ID'), _t('用户 ID 在个人主页中查询，是 "你的用户名 @" 后面的那串数字'));
+        $userID->input->setAttribute('class', 'mini');
+        $form->addInput($userID);
+        $appID = new Typecho_Widget_Helper_Form_Element_Text('appID', NULL, NULL, _t('输入你的 app_id'), _t('app_id 在开发者平台中申请，https://bangumi.tv/dev/app'));
+        $appID->input->setAttribute('class', 'mini1');
+        $form->addInput($appID);
+        $hasCache = new Typecho_Widget_Helper_Form_Element_Radio('hasCache', array('1' => _t('开启'), '0' => _t('关闭')), '0', _t('开启缓存'), _t('开启缓存需保证插件根目录可写'));
+        $form->addInput($hasCache);
+        $cacheTime = new Typecho_Widget_Helper_Form_Element_Text('cacheTime', NULL, '86400', _t('缓存过期时间'), _t('设置缓存过期时间，单位为秒，默认为一天'));
+        $form->addInput($cacheTime);
+    }
+
+    public static function replace($content, $widget, $lastResult)
+    {
+        $content = empty($lastResult) ? $content : $lastResult;
+        if (strpos($content, '[bangumi]') !== false) {
+            $content = str_replace("[bangumi]", self::output(), $content);
+        }
+        return $content;
+    }
+
+    public static function output()
+    {
+        //$cssPath = Helper::options()->pluginUrl . '/BangumiList/loading.css';
+        return '<style>.bangumi_loading{width:150px;height:150px;border-radius:50%;perspective:800px;margin:20px auto}.inner{position:absolute;box-sizing:border-box;width:100%;height:100%;border-radius:50%}.inner.one{left:0;top:0;animation:rotate-one 1s linear infinite;border-bottom:5px solid #ff73b3}.inner.two{right:0;top:0;animation:rotate-two 1s linear infinite;border-right:5px solid #ff73b3}.inner.three{right:0;bottom:0;animation:rotate-three 1s linear infinite;border-top:5px solid #ff73b3}@keyframes rotate-one{0%{transform:rotateX(35deg) rotateY(-45deg) rotateZ(0deg)}100%{transform:rotateX(35deg) rotateY(-45deg) rotateZ(360deg)}}@keyframes rotate-two{0%{transform:rotateX(50deg) rotateY(10deg) rotateZ(0deg)}100%{transform:rotateX(50deg) rotateY(10deg) rotateZ(360deg)}}@keyframes rotate-three{0%{transform:rotateX(35deg) rotateY(55deg) rotateZ(0deg)}100%{transform:rotateX(35deg) rotateY(55deg) rotateZ(360deg)}}.bangumiList{margin:20px 0}.bangumi_loading_text{text-align:center;font-size:16px;font-weight:600;color:#c7254e}.Bangumi ul{display:inline-block;vertical-align:top;margin:10px 10px 10px 0}.Bangumi ul img{width:88px;height:132px;object-fit:cover}.Bangumi ul p{width:88px;display:block;margin-block-start:1em;margin-block-end:1em;margin-inline-start:0;margin-inline-end:0;overflow-wrap:break-word}</style>' .
+            '<div id="Bangumi" class="Bangumi">
+        	    <div>
+                    <div class="bangumi_loading">
+                        <div class="inner one"></div>
+                        <div class="inner two"></div>
+                        <div class="inner three"></div>
+                        </div>
+                    <div class="bangumi_loading_text">追番列表加载中...</div>
+                </div>
+            </div>' . "
+		<script>
+		var xhrbgm = new XMLHttpRequest();
+        xhrbgm.open('GET', '/BangumiList', true);
+        xhrbgm.send();
+        xhrbgm.onreadystatechange = function (e) {
+          if (xhrbgm.readyState == 4 && xhrbgm.status == 200) {
+            Bangumi.innerHTML = '';
+            Bangumi.insertAdjacentHTML('beforeend', xhrbgm.responseText);
+            //console.log(xhrbgm.responseText);
+          }else{
+            Bangumi.insertAdjacentHTML('beforeend', '<p>追番列表加载失败</p>');
+          }
+        };
+		</script>";
+    }
+}
